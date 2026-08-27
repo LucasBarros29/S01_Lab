41179a94399030cf10e18f5f13168dd7156d7b6b
Lucas Barros <lucasbtrabalhos29@gmail.com>
2026-08-27 16:19:57 -0300

    Update exercicio_2.lua

--- S01_Relatório/Relatorio-02/Exercicio-02/exercicio_2.lua
+++ S01_Relatório/Relatorio-02/Exercicio-02/exercicio_2.lua
@@ -1,1 +1,28 @@
+function contarOcorrencias(tabela, alvo)
+    local contador = 0
 
+    for i = 1, #tabela do
+        if tabela[i] == alvo then
+            contador = contador + 1
+        end
+    end
+
+    return contador
+end
+
+print("Digite a quantidade de elementos da tabela:")
+local N = tonumber(io.read())
+
+local numeros = {}
+
+for i = 1, N do
+    print("Digite o elemento " .. i .. ":")
+    numeros[i] = tonumber(io.read())
+end
+
+print("Digite o número a ser buscado:")
+local X = tonumber(io.read())
+
+local quantidade = contarOcorrencias(numeros, X)
+
+print("O número " .. X .. " aparece " .. quantidade .. " vez(es) na tabela.")
