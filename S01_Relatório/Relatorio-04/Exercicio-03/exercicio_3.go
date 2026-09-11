c8dcec501209da190aec2f76ae330d68fe92b2d6
Lucas Barros <lucasbtrabalhos29@gmail.com>
2026-09-10 21:20:57 -0300

    Implement plantão schedule display function

--- S01_Relatório/Relatorio-04/Exercicio-03/exercicio_3.go
+++ S01_Relatório/Relatorio-04/Exercicio-03/exercicio_3.go
@@ -1,1 +1,23 @@
+package main
 
+import "fmt"
+
+func criarEscalaTecnicos(totalPlantoes int) {
+	fmt.Println("--- Escala de Plantao Tecnico ---")
+
+	// Percorre a quantidade de plantões informada e calcula os dias da escala
+	for indice := 0; indice < totalPlantoes; indice++ {
+		diaPlantao := 1 + indice*4
+		fmt.Printf("Plantao %d: Dia %d do mes\n", indice+1, diaPlantao)
+	}
+}
+
+func main() {
+	var numeroPlantoes int
+
+	fmt.Print("Digite a quantidade de plantoes necessarios: ")
+	fmt.Scanln(&numeroPlantoes)
+
+	// Chama a função responsável por exibir a escala de plantões
+	criarEscalaTecnicos(numeroPlantoes)
+}
