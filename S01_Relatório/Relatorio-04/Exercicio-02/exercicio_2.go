cc6a1294e4b9ba23495ccbb310334310bdf49dd9
Lucas Barros <lucasbtrabalhos29@gmail.com>
2026-09-10 21:19:41 -0300

    Add sales input and classification logic

--- S01_Relatório/Relatorio-04/Exercicio-02/exercicio_2.go
+++ S01_Relatório/Relatorio-04/Exercicio-02/exercicio_2.go
@@ -1,1 +1,34 @@
+package main
 
+import "fmt"
+
+func main() {
+	var vendasQ1, vendasQ2, vendasQ3 int
+
+	fmt.Print("Digite as vendas do 1o trimestre: ")
+	fmt.Scanln(&vendasQ1)
+
+	fmt.Print("Digite as vendas do 2o trimestre: ")
+	fmt.Scanln(&vendasQ2)
+
+	fmt.Print("Digite as vendas do 3o trimestre: ")
+	fmt.Scanln(&vendasQ3)
+
+	somaVendas := vendasQ1 + vendasQ2 + vendasQ3
+
+	if somaVendas < 100 {
+		fmt.Println("Meta minima anual nao atingida!")
+	} else {
+		fmt.Println("Total de vendas:", somaVendas, "unidades")
+
+		// Avalia a categoria de acordo com o total acumulado de vendas
+		switch {
+		case somaVendas >= 250:
+			fmt.Println("Classificacao: Categoria Top Seller")
+		case somaVendas >= 180:
+			fmt.Println("Classificacao: Categoria Senior")
+		default:
+			fmt.Println("Classificacao: Categoria Pleno")
+		}
+	}
+}
