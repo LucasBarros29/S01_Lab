692e06448a54a02ed7a7c547622498e21a17856e
Lucas Barros <lucasbtrabalhos29@gmail.com>
2026-09-10 21:24:17 -0300

    Implement access validation for VIP area

--- S01_Relatório/Relatorio-04/Exercicio-04/exercicio_4.go
+++ S01_Relatório/Relatorio-04/Exercicio-04/exercicio_4.go
@@ -1,1 +1,47 @@
+package main
 
+import "fmt"
+
+func conferirAcesso(area string, numeroIngresso int) bool {
+	if area == "VIP" && numeroIngresso == 2026 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	var areaEvento string
+	var codigoAcesso int
+
+	// Repete a validação até que um ingresso válido seja informado
+	for {
+		fmt.Print("Digite o setor do ingresso: ")
+
+		_, erroArea := fmt.Scanln(&areaEvento)
+
+		// Caso ocorra erro na leitura, o programa é finalizado
+		if erroArea != nil {
+			fmt.Println()
+			fmt.Println("Entrada encerrada.")
+			break
+		}
+
+		fmt.Print("Digite o codigo do ingresso: ")
+
+		_, erroCodigo := fmt.Scanln(&codigoAcesso)
+
+		// Verifica se o código foi informado corretamente
+		if erroCodigo != nil {
+			fmt.Println()
+			fmt.Println("Entrada encerrada.")
+			break
+		}
+
+		if conferirAcesso(areaEvento, codigoAcesso) {
+			fmt.Println("Acesso liberado a area VIP!")
+			break
+		} else {
+			fmt.Println("Ingresso ou setor invalido. Tente novamente.")
+		}
+	}
+}
